a58158f77ec7e32b8366297dc7933b3c23e6eb66
Masanori  Hirano <masanori@jm00z2cs43.mnj.pfn.io>
2023-11-13 03:01:32 +0000

    bug fix

--- developments/results_aggregator.py
+++ developments/results_aggregator.py
@@ -5,6 +5,10 @@ from collections import defaultdict
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Union
+from typing import cast
+
+from sklearn import base
 
 
 def get_best_metric(
@@ -17,66 +21,56 @@ def get_best_metric(
         "security_sales_1": ["acc", "map", "map_2", "map_3", "map_4"],
     },
     order_dict: Dict[str, Dict[str, str]] = {},
-):
+) -> Dict:
     best_metric_dict: Dict[str, Any] = defaultdict(
         lambda: {"metric_values": None, "n": -1, "key": ""}
     )  # Initialize best metric dict
+    results_data: Dict[
+        str, List[Dict[str, Union[int, str, Dict[str, float]]]]
+    ] = defaultdict(list)
+    json_files = glob.glob(
+        f"{results_path}-*.json"
+    )  # Read all json files with the given prefix
+    for json_file in json_files:
+        with open(json_file, "r") as f:
+            data = json.load(f)  # Load the data
+        for (key, results), few_shot in zip(
+            cast(Dict[str, Dict[str, float]], data["results"]).items(),
+            cast(List[int], data["config"]["num_fewshot"]),
+        ):
+            base_keys = key.split("-")
+            base_name = base_keys[0]
+            if base_name not in prefix_list:
+                continue
+            results_data[base_name].append(
+                {"num_few_shot": few_shot, "results": results, "key": key}
+            )
 
     for prefix in prefix_list:
-        json_files = glob.glob(
-            f"{results_path}-*.json"
-        )  # Read all json files with the given prefix
-        for json_file in json_files:
-            with open(json_file, "r") as f:
-                data = json.load(f)  # Load the data
-
-            for key, results in data["results"].items():
-                base_keys = key.split("-")
-                base_name = base_keys[0]
-
-                metrics = metric_dict[base_name]
-
-                # Set order for this key, if none specified, set it to "desc"
-                _order_dict = order_dict.get(base_name, {})
-                orders = [_order_dict.get(metric, "desc") for metric in metrics]
-
-                metric_values = [results[metric] for metric in metrics]
-
-                for i in range(len(metrics)):
-                    if (
-                        (best_metric_dict[base_name]["metric_values"] is None)
-                        or (
-                            orders[i] == "asc"
-                            and metric_values[i]
-                            < best_metric_dict[base_name]["metric_values"][i]
-                        )
-                        or (
-                            orders[i] == "desc"
-                            and metric_values[i]
-                            > best_metric_dict[base_name]["metric_values"][i]
-                        )
-                    ):  # If the current performance metric is better than the best, update it
-                        best_metric_dict[base_name]["metric_values"] = metric_values
-                        best_metric_dict[base_name]["n"] = int(
-                            json_file.split("-")[-1].split(".")[0]
-                        )  # Get the index of the best score
-                        best_metric_dict[base_name]["key"] = key
-                        break
-                    elif (
-                        orders[i] == "asc"
-                        and metric_values[i]
-                        > best_metric_dict[base_name]["metric_values"][i]
-                    ) or (
-                        orders[i] == "desc"
-                        and metric_values[i]
-                        < best_metric_dict[base_name]["metric_values"][i]
-                    ):
-                        break
+        target_data: List[Dict[str, Union[int, str, Dict[str, float]]]] = results_data[
+            prefix
+        ]
+        metrics = metric_dict[prefix]
+        orders = [order_dict.get(metric, "desc") for metric in metrics]
+        target_data = sorted(
+            target_data,
+            key=lambda x: [
+                cast(Dict[str, float], x["results"])[metric]
+                * (-1.0 if order == "asc" else 1.0)
+                for metric, order in zip(metrics, orders)
+            ],
+            reverse=True,
+        )
+        best_metric_dict[prefix]["metric_values"] = cast(
+            Dict[str, float], target_data[0]["results"]
+        )[metrics[0]]
+        best_metric_dict[prefix]["n"] = target_data[0]["num_few_shot"]
+        best_metric_dict[prefix]["key"] = target_data[0]["key"]
 
     return best_metric_dict
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description="Compute the best metrics")
     parser.add_argument(
         "--results_path",
@@ -101,9 +95,13 @@ def main():
     # Parse best metrics into output format
     task = ",".join([str(best_metrics[prefix]["key"]) for prefix in prefix_list])
     num_fewshot = ",".join([str(best_metrics[prefix]["n"]) for prefix in prefix_list])
+    values = ",".join(
+        [str(best_metrics[prefix]["metric_values"]) for prefix in prefix_list]
+    )
 
     print(f'TASK="{task}"')
     print(f'--num_fewshot "{num_fewshot}"')
+    print(f"{values}")
 
 
 if __name__ == "__main__":
