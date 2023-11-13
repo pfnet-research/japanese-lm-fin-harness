5fb9016ac9094b228a875420951faa2440f28450
Masanori  Hirano <masanori@jm00z2cs43.mnj.pfn.io>
2023-11-13 02:02:18 +0000

    added leaderboard

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # japanese-lm-fin-harness
+
+## Leaderboard
+<!-- lb start -->
+| Model | Ave | ave | chabsa | cma_basics | cpa | security_sales_1 |
+| --- | --- | --- | --- | --- | --- | --- |
+| [stabilityai/japanese-stablelm-instruct-alpha-7b-v2](https://huggingface.co/stabilityai/japanese-stablelm-instruct-alpha-7b-v2) | 39.29 | 50.85 | 36.84 | 18.59 | 50.88 |
+| [pfnet/plamo-13b](https://huggingface.co/pfnet/plamo-13b) | 35.78 | 50.97 | 28.95 | 15.83 | 47.37 |
+| [cyberagent/open-calm-1b](https://huggingface.co/cyberagent/open-calm-1b) | 32.15 | 37.06 | 26.32 | 21.36 | 43.86 |
+| [line-corporation/japanese-large-lm-3.6b](https://huggingface.co/line-corporation/japanese-large-lm-3.6b) | 28.68 | 38.25 | 26.32 | 16.83 | 33.33 |
+| [llm-jp/llm-jp-13b-v1.0](https://huggingface.co/llm-jp/llm-jp-13b-v1.0) | 26.55 | 23.96 | 26.32 | 13.82 | 42.11 |
+<!-- lb end -->
---
+++ developments/make_leaderboard.py
@@ -0,0 +1,40 @@
+import json
+import glob
+
+result_dict = {}
+
+for file_name in glob.glob("models/*/*/result.json"):
+    _, company, modelname, _ = file_name.split("/")
+    company_model = company + "/" + modelname
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    results_sum = 0
+    count = 0
+    for task, results in data["results"].items():
+        task_name = task.split('-')[0]
+        result = results.get("f1", results.get("acc"))
+        result = 100 * (result if result else 0)
+        result_dict.setdefault(company_model, {})[task_name] = result
+        if result:
+            results_sum += result
+            count += 1
+    result_dict.setdefault(company_model, {})['ave'] = results_sum / count if count > 0 else 0
+
+sorted_results = sorted(result_dict.items(), key=lambda x: x[1].get('ave', 0), reverse=True)
+
+first_row = "| Model | Ave | " + ' | '.join(sorted(next(iter(result_dict.values())))) + " |\n| --- | --- | " + ' | '.join(['---'] * len(next(iter(result_dict.values())))) + " |\n"
+md_table = first_row
+for company_model, results in sorted_results:
+    md_table += "| [" + company_model + "](https://huggingface.co/" + company_model.replace("/", "/") + ") | " + "{:.2f}".format(results.pop('ave')) + " | " + ' | '.join("{:.2f}".format(results.get(task_name, "N/A")) for task_name in sorted(results)) + " |\n"
+
+with open("README.md", "r") as f:
+    readme_content = f.read()
+
+start_index = readme_content.index("<!-- lb start -->") + len("<!-- lb start -->")
+end_index = readme_content.index("<!-- lb end -->")
+new_readme_content = readme_content[:start_index] + "\n" + md_table + readme_content[end_index:]
+
+with open("README.md", "w") as f:
+    f.write(new_readme_content)
+
+print("Leader Board was written in README.md")
