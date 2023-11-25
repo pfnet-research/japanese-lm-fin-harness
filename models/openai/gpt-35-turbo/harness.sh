4d455122643f868363f295adb8f1b14923a56d4a
Masanori HIRANO <masa.hirano.1996@gmail.com>
2023-11-25 13:33:03 +0900

    rename

--- models/openai/gpt-35-turbo/harness.sh
+++
@@ -1,3 +0,0 @@
-TASK="chabsa,cma_basics,cpa_audit,security_sales_1,fp2"
-python open_ai.py --model gpt-35-turbo --tasks $TASK --num_fewshot 0 --output_path "models/openai/gpt-35-turbo/result.json"
-# azure 2023-03-15-preview 
---
+++ models/openai/gpt-4-0613/harness.sh
@@ -0,0 +1,3 @@
+TASK="chabsa,cma_basics,cpa_audit,security_sales_1,fp2"
+python open_ai.py --model gpt-4-0613 --tasks $TASK --num_fewshot 0 --output_path "models/openai/gpt-4-0613/result.json"
+# azure 2023-03-15-preview (0613)
