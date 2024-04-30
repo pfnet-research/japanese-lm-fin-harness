ba46e6e08c0ade20761040577f70da2a6677e821
Masanori HIRANO <masa.hirano.1996@gmail.com>
2024-04-30 15:50:30 +0900

    add

--- models/stabilityai/japanese-stablelm-base-gamma-7b/harness-1.0-0.2.sh
+++ models/stabilityai/japanese-stablelm-base-gamma-7b/harness-1.0-0.2.sh
@@ -1,4 +1,4 @@
-MODEL_ARGS="pretrained=stabilityai/japanese-stablelm-base-gamma-7b,trust_remote_code=True,tokenizer=novelai/nerdstash-tokenizer-v1,dtype=bfloat16"
+MODEL_ARGS="pretrained=stabilityai/japanese-stablelm-base-gamma-7b,trust_remote_code=True,dtype=bfloat16"
 TASK="chabsa-1.0-0.2,cma_basics-1.0-0.2,cpa_audit-1.0-0.2,fp2-1.0-0.2,security_sales_1-1.0-0.2"
 python main.py --model vllm --model_args $MODEL_ARGS --tasks $TASK --num_fewshot 0 --output_path "models/stabilityai/japanese-stablelm-base-gamma-7b/result-1.0-0.2.json"
 # a100-80gb: 1
--- models/stabilityai/japanese-stablelm-instruct-gamma-7b/harness-1.0-0.1.sh
+++ models/stabilityai/japanese-stablelm-instruct-gamma-7b/harness-1.0-0.1.sh
@@ -1,4 +1,4 @@
-MODEL_ARGS="pretrained=stabilityai/japanese-stablelm-instruct-gamma-7b,trust_remote_code=True,tokenizer=novelai/nerdstash-tokenizer-v1,dtype=bfloat16"
+MODEL_ARGS="pretrained=stabilityai/japanese-stablelm-instruct-gamma-7b,trust_remote_code=True,dtype=bfloat16"
 TASK="chabsa-1.0-0.1,cma_basics-1.0-0.1,cpa_audit-1.0-0.1,fp2-1.0-0.1,security_sales_1-1.0-0.1"
 python main.py --model vllm --model_args $MODEL_ARGS --tasks $TASK --num_fewshot 0 --output_path "models/stabilityai/japanese-stablelm-instruct-gamma-7b/result-1.0-0.1.json"
 # a100-80gb: 1
