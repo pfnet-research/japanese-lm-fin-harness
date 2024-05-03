0efcb32069fe74ad62f2de1c693586d761180466
Masanori HIRANO <masa.hirano.1996@gmail.com>
2024-05-03 22:51:53 +0900

    add

--- models/openai/gpt-35-turbo/harness-default.sh
+++ models/openai/gpt-35-turbo/harness-default.sh
@@ -1,3 +1,3 @@
 TASK="chabsa,cma_basics,cpa_audit,security_sales_1,fp2"
-python open_ai.py --model_args model=gpt-35-turbo --tasks $TASK --num_fewshot 0 --output_path "models/openai/gpt-35-turbo/result.json"
+python api_models.py --model openai --model_args model=gpt-35-turbo --tasks $TASK --num_fewshot 0 --output_path "models/openai/gpt-35-turbo/result-default.json"
 # azure 0613: Data up to Sep 2021
