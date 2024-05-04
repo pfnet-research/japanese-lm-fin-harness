3e4b69f31f346c21e233d8d603ad473e6509896d
Masanori HIRANO <masa.hirano.1996@gmail.com>
2024-05-04 12:25:07 +0900

    add

--- models/anthropic/claude-3-haiku/harness-default.sh
+++ models/anthropic/claude-3-haiku/harness-default.sh
@@ -1,2 +1,2 @@
 TASK="chabsa,cma_basics,cpa_audit,security_sales_1,fp2"
-python api_models.py --model anthropic --model_args model=claude-3-haiku-20240307 --tasks $TASK --num_fewshot 0 --output_path "models/anthropic/claude-3-haiku/result-default.json" --limit 10  --use_cache ./cache/claude-3-haiku/
+python api_models.py --model anthropic --model_args model=claude-3-haiku-20240307 --tasks $TASK --num_fewshot 0 --output_path "models/anthropic/claude-3-haiku/result-default.json" --use_cache ./cache/claude-3-haiku/
