0023bf875c777e85798ae97fd6e204dcc7bd83a6
Masanori Hirano <masa.hirano.1996@gmail.com>
2024-10-28 21:07:18 +0900

    update to lm_eval 0.4.5

--- jlm_fin_eval/tasks/__init__.py
+++ jlm_fin_eval/tasks/__init__.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import List, Optional, Union
 
 from lm_eval.tasks import TaskManager as OriginalTaskManager
 
@@ -9,17 +9,25 @@ class TaskManager(OriginalTaskManager):
     and an optional directory if provided.
 
     """
-
-    def initialize_tasks(self, include_path: Optional[str] = None) -> dict:
+    def initialize_tasks(
+        self,
+        include_path: Optional[Union[str, List]] = None,
+        include_defaults: bool = True,
+    ):
         """Creates a dictionary of tasks index.
 
-        :param include_path: str = None
-            An additional path to be searched for tasks
-
+        :param include_path: Union[str, List] = None
+            An additional path to be searched for tasks recursively.
+            Can provide more than one such path as a list.
+        :param include_defaults: bool = True
+            If set to false, default tasks (those in lm_eval/tasks/) are not indexed.
         :return
             Dictionary of task names as key and task metadata
         """
-        all_paths = [os.path.dirname(os.path.abspath(__file__)) + "/"]
+        if include_defaults:
+            all_paths = [os.path.dirname(os.path.abspath(__file__)) + "/"]
+        else:
+            all_paths = []
         if include_path is not None:
             if isinstance(include_path, str):
                 include_path = [include_path]
